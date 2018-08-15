e0784ecfc641395bb97690221dd00a69de284075
root <root@eivindlia1.mylabserver.com>
2018-08-15 15:13:05 +0000

    fix typos

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class nginx (
+class nginx(
   String $ensure                        = $::nginx::params::ensure,
   String $package_name                  = $::nginx::params::package_name,
   String $config_dir			= $::nginx::params::config_dir,
@@ -10,21 +10,17 @@ class nginx (
   String $config_log_dir		= $::nginx::params::config_log_dir,
   String $config_pid_file		= $::nginx::params::config_pid_file,
   Optional[String] $config_vdir_enable	= $::nginx::params::config_vdir_enable,
-  
   String $config_process_user		= $::nginx::params::config_process_user,
   String $service_pattern		= $::nginx::params::service_pattern,
   String $service_ensure		= $::nginx::params::service_ensure,
   String $service_name			= $::nginx::params::service_name,
-
   Boolean $service_enable		= $::nginx::params::service_enable,
   Boolean $service_hasstatus		= $::nginx::params::service_hasstatus,
   Boolean $service_hasrestart		= $::nginx::params::service_hasrestart,
-
   String $docroot			= $::nginx::params::docroot,
 
 ) inherits ::nginx::params {
   class { '::nginx::install': }
   -> class { '::nginx::config': }
-  -> class { '::nginx::service': }
-
+  ~> class { '::nginx::service': }
 }
